7960450f28be86e740a4ca589fc647c3ca808d0f
Amal Hussein <amalhussein08@gmail.com>
2013-10-31 00:02:54 -0400

    working on extra credit

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ CourseApp.Student.prototype.generate_html();
 // ===========  TEACHER ==============
 CourseApp.Teacher = function(name) { 
 };
+
 CourseApp.Teacher.prototype.generate_html = function(){ 
 
 	var teachers = ['Billy', 'Neesha', 'Samara', 'Geesha'], 
@@ -40,10 +41,8 @@ CourseApp.Teacher.prototype.generate_html();
 // =========== COURSE ==============
 CourseApp.Course = function(name) {
 	this.name = name; 
-      };
+};
 
-// This will give all the courses access to a method .generate_html
-// which you can use to render each student to the page indvidiually
 CourseApp.Course.prototype.generate_html = function(){ 
 
 	var courses = ['physics', 'chemistry', 'math', 'computer science'], 
@@ -66,37 +65,28 @@ CourseApp.Course.prototype.generate_html();
 //                                                       //
 ///////////////////////////////////////////////////////////
 
-
-// The following 3 methods will run ONLY when you click the 
-// 'Add Foo' button in the HTML. Ignore what the 'e' is in each
-// of the methods
-
-// For example, if I click the 'Add Teacher' button on the page
-// the 'CourseApp.add_teacher' method will run
-
 CourseApp.add_teacher = function(e){
 
 	name = prompt('Whats your teachers name?');
 	teachers.push(new CourseApp.Teacher(name));
-  // Prompt the user for information to add a teacher
-  // Append this teacher to the list of teachers on the page
 };
 
-CourseApp.add_teacher();
+CourseApp.add_teacher(e);
 
 CourseApp.add_course = function(e){
 	name = prompt('Whats your students name?');
 	courses.push(new CourseApp.Course(name));
 };
 
-CourseApp.add_course();
+CourseApp.add_course(e);
 
 CourseApp.add_student = function(e){
 
-	name = prompt('Whats your students name?');
+	e = prompt('Whats your students name?');
 	students.push(new CourseApp.Student(name));
-  // Prompt the user for information to add a student
-  // Append this student to the list of students on the page
 };
 
-CourseApp.add_student();
+CourseApp.add_student(e);
+
+
+
