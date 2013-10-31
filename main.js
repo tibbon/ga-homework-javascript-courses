a8e960bd08e7c7d39ad191da914dacae577aaf25
Michael Finneran <mrfinneran@gmail.com>
2013-10-30 20:42:36 -0400

    out student, courses, and teachers onto html

--- main.js
+++ main.js
@@ -3,60 +3,101 @@ var CourseApp = {};
 
 // =========== STUDENT ==============
 CourseApp.Student = function(name) {
-                 // a student object properties
+       this.name = name;
 }
 
+p1 = new CourseApp.Student('Emma');
+p2 = new CourseApp.Student('Abby');
+p3 = new CourseApp.Student('Cori');
+p4 = new CourseApp.Student('Amal');
+var people = [p1, p2, p3, p4]
 // This will give all the students access to a method .generate_html
 // which you can use to render each student to the page indvidiually
-CourseApp.Student.prototype.generate_html = function(){ ...}
+CourseApp.Student.prototype.generate_html = function(people){
+	var i = 0;
+	max = people.length;
+	students = document.getElementById('students').innerHTML;
+	for(; i < max;) {
+		students += "<li class='student-list'>" + people[i].name + "</li>";
+		i += 1;
+	}
+	document.getElementById('students').innerHTML = students;
+}
 
-
+CourseApp.Student.prototype.generate_html(people);
 // ===========  TEACHER ==============
 CourseApp.Teacher = function(name) {
-                 // a teacher object properties
+           this.name = name;
 }
+
+s1 = new CourseApp.Teacher('Teddy');
+s2 = new CourseApp.Teacher('Tom');
+s3 = new CourseApp.Teacher('David');
+s4 = new CourseApp.Teacher('Matt');
+var teachers = [s1, s2, s3, s4]
 // This will give all the teachers access to a method .generate_html
 // which you can use to render each student to the page indvidiually
-CourseApp.Teacher.prototype.generate_html = function(){ ...}
+CourseApp.Teacher.prototype.generate_html = function(teachers){
+	var i = 0;
+	max = teachers.length;
+	teacherHTML = document.getElementById('teachers').innerHTML;
+	for(; i < max;) {
+		teacherHTML += "<li class='teacher-list'>" + teachers[i].name + "</li>";
+		i += 1;
+	}
+	document.getElementById('teachers').innerHTML = teacherHTML;
+}
+
+CourseApp.Teacher.prototype.generate_html(teachers);
 
 // =========== COURSE ==============
 CourseApp.Course = function(name) {
-                 // a course object properties
+           this.name = name;
 }
 
+c1 = new CourseApp.Course('JavaScript');
+c2 = new CourseApp.Course('Ruby');
+c3 = new CourseApp.Course('HTML');
+c4 = new CourseApp.Course('CSS');
+var courses = [c1, c2, c3, c4]
 // This will give all the courses access to a method .generate_html
 // which you can use to render each student to the page indvidiually
-CourseApp.Course.prototype.generate_html = function(){ ...}
-
-
-
-
-
-///////////////////////////////////////////////////////////
-//                                                       //
-//            BONUS STAGE BELOW                          //
-//                                                       //
-///////////////////////////////////////////////////////////
-
-
-// The following 3 methods will run ONLY when you click the 
-// 'Add Foo' button in the HTML. Ignore what the 'e' is in each
-// of the methods
-
-// For example, if I click the 'Add Teacher' button on the page
-// the 'CourseApp.add_teacher' method will run
-
-CourseApp.add_teacher = function(e){
-  // Prompt the user for information to add a teacher
-  // Append this teacher to the list of teachers on the page
+CourseApp.Course.prototype.generate_html = function(){ 	
+	var i = 0;
+	max = courses.length;
+	courseHTML = document.getElementById('courses').innerHTML;
+	for(; i < max;) {
+		courseHTML += "<li class='course-list'>" + courses[i].name + "</li>";
+		i += 1;
+	}
+	document.getElementById('courses').innerHTML = courseHTML;
 }
+CourseApp.Course.prototype.generate_html(courses);
+// ///////////////////////////////////////////////////////////
+// //                                                       //
+// //            BONUS STAGE BELOW                          //
+// //                                                       //
+// ///////////////////////////////////////////////////////////
 
-CourseApp.add_course = function(e){
-  // Prompt the user for information to add a course
-  // Append this course to the list of courses on the page
-}
 
-CourseApp.add_student = function(e){
-  // Prompt the user for information to add a student
-  // Append this student to the list of students on the page
-}
+// // The following 3 methods will run ONLY when you click the 
+// // 'Add Foo' button in the HTML. Ignore what the 'e' is in each
+// // of the methods
+
+// // For example, if I click the 'Add Teacher' button on the page
+// // the 'CourseApp.add_teacher' method will run
+
+// CourseApp.add_teacher = function(e){
+//   // Prompt the user for information to add a teacher
+//   // Append this teacher to the list of teachers on the page
+// }
+
+// CourseApp.add_course = function(e){
+//   // Prompt the user for information to add a course
+//   // Append this course to the list of courses on the page
+// }
+
+// CourseApp.add_student = function(e){
+//   // Prompt the user for information to add a student
+//   // Append this student to the list of students on the page
+// }
